91a6c40d7bc1124260f9d72a5b6591ed6c927d14
NicoRobine <472150631@qq.com>
2021-10-18 09:56:32 +0800

    修复通道bug

--- ios/Classes/FlutterPjsipPlugin.m
+++ ios/Classes/FlutterPjsipPlugin.m
@@ -26,22 +26,61 @@
 
 @implementation FlutterPjsipPlugin
 + (void)registerWithRegistrar:(NSObject<FlutterPluginRegistrar>*)registrar {
-  FlutterMethodChannel* channel = [FlutterMethodChannel
+    FlutterMethodChannel* channel = [FlutterMethodChannel
       methodChannelWithName:@"flutter_pjsip"
             binaryMessenger:[registrar messenger]];
-    [self methodChannel:channel];
-  FlutterPjsipPlugin* instance = [[FlutterPjsipPlugin alloc] init];
-  [registrar addMethodCallDelegate:instance channel:channel];
+//    [self methodChannel:channel];
+    FlutterPjsipPlugin* instance = [[FlutterPjsipPlugin alloc] init];
+    [registrar addMethodCallDelegate:instance channel:channel];
 }
 
 - (void)handleMethodCall:(FlutterMethodCall*)call result:(FlutterResult)result {
-  if ([@"getPlatformVersion" isEqualToString:call.method]) {
-    result([@"iOS " stringByAppendingString:[[UIDevice currentDevice] systemVersion]]);
-  } else {
-    result(FlutterMethodNotImplemented);
-  }
+    [PJSipManager manager].methodChannel = methodChannel;
+    NSString *method=call.method;
+    NSDictionary * dict = (NSDictionary *)call.arguments;
+    if ([@"getPlatformVersion" isEqualToString:call.method]) {
+        result([@"iOS " stringByAppendingString:[[UIDevice currentDevice] systemVersion]]);
+    } else if ([method isEqualToString:method_pjsip_init]) {/** 初始化*/
+        [PJSipManager manager];
+        result(@(YES));
+    } else if ([method isEqualToString:method_pjsip_login]) {/** 登录*/
+        NSLog(@"登录名称：%@",[dict objectForKey:@"username"]);
+        if ([[PJSipManager manager] registerAccountWithName:[dict objectForKey:@"username"] password:[dict objectForKey:@"password"] IPAddress:[NSString stringWithFormat:@"%@:%@",[dict objectForKey:@"ip"],[dict objectForKey:@"port"]]]) {
+            result(@(YES));
+        }else{
+             result(@(NO));
+        }
+    } else if ([method isEqualToString:method_pjsip_call]) {/** 拨打电话*/
+        NSLog(@"拨打的电话号码：%@",[dict objectForKey:@"username"]);
+        [[PJSipManager manager] dailWithPhonenumber:[dict objectForKey:@"username"]];
+        result(@(YES));
+    } else if ([method isEqualToString:method_pjsip_receive]) {/** 接收电话*/
+        [[PJSipManager manager] incommingCallReceive];
+        result(@(YES));
+    } else if ([method isEqualToString:method_pjsip_hands_free]) {/** 免提*/
+        [[PJSipManager manager]setAudioSession];
+        result(@(YES));
+    } else if ([method isEqualToString:method_pjsip_mute]) {/** 静音*/
+        [[PJSipManager manager] muteMicrophone];
+        result(@(YES));
+    } else if ([method isEqualToString:method_pjsip_refuse]) {/** 挂断&&拒接*/
+        [[PJSipManager manager]hangup];
+        result(@(YES));
+    } else if ([method isEqualToString:method_pjsip_logout]) {/** 登出*/
+        if ([[PJSipManager manager]logOut]) {
+            result(@(YES));
+        }else{
+            result(@(NO));
+        }
+    } else if ([method isEqualToString:method_pjsip_deinit]) {/** 销毁*/
+        [PJSipManager attempDealloc];
+        result(@(YES));
+    } else {
+        result(FlutterMethodNotImplemented);
+    }
 }
 
+// 废弃
 + (void)methodChannel:(FlutterMethodChannel *)methodChannel{
     
     [PJSipManager manager].methodChannel = methodChannel;
